b94da4b5821982bae5c78e0edb838a08f382ca76
sinazarrin <goldmontain8@gmail.com>
2023-02-23 19:02:27 +0330

    Create pages and display them

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,14 @@
+import { Col, Row } from "react-bootstrap"
+
+const Store = () => {
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        
+      </Row>
+    </>
+  )
+}
+
+export default Store
